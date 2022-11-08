358770fc3394823802279867e58f3e8154ce1803
ryota0603 <114867300+ryota0603@users.noreply.github.com>
2022-11-08 03:18:33 +0000

    +coment,destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   
   resources :post_images, only: [:new, :create,:index, :show,:destroy]do
-    resources :post_comments, only: [:create]
+    resources :post_comments, only: [:create,:destroy]
   end
   
   resources :users, only: [:show, :edit,:update]
